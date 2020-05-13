de7c5bdf2df57519143ce65f6574bfbd835caac3
kimjbstar <kimjbstar@gmail.com>
2020-05-13 12:00:07 +0900

    404 page 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Admin from './admin/routes'
-import Common from './common/routes'
+import Vue from "vue";
+import Router from "vue-router";
+import Admin from "./admin/routes";
+import Common from "./common/routes";
+import PageNotFound from "../views/Common/PageNotFound";
 
-Vue.use(Router)
+Vue.use(Router);
 
-const routes = [
-  Admin,
-  Common
-]
+const routes = [Admin, Common, { path: "*", component: PageNotFound }];
 
 const router = new Router({
-  mode: 'history',
+  mode: "history",
   // base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
